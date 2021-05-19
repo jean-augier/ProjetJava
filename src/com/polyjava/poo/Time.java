1e16ee5ed967ccb34eae5269dade1e2d02f03e7e
Axel <axelraballand3@gmail.com>
2021-05-19 16:23:37 +0200

    Time

--- src/com/polyjava/poo/Time.java
+++ src/com/polyjava/poo/Time.java
@@ -9,48 +9,81 @@ public class Time {
 
 	private LocalTime hour;
 
-	public void Time() {
-
+	public Time() {
+		date = LocalDate.now();
+		hour = LocalTime.now();
 	}
 
 	public Time(LocalTime hour) {
-
+		date = LocalDate.now();
+		this.hour = hour;
 	}
 
 	public Time(LocalDate date) {
-
+		this.date = date;
+		hour = LocalTime.now();
 	}
 
 	public Time(LocalTime hour, LocalDate date ) {
-
+		this.date = date;
+		this.hour = hour;
 	}
 
 	public LocalDate getDate() {
-		return null;
+		return date;
 	}
 
-	public void setDate() {
-
+	public void setDate(LocalDate date) {
+		this.date = date;
 	}
 
 	public LocalTime getHour() {
-		return null;
+		return hour;
 	}
 
 	public LocalTime getRoundedHour() {
-		return null;
+		return this.getRoundedHour(this.hour);
 	}
 
 	public LocalTime getRoundedHour(LocalTime hour) {
-		return null;
+		int newMinute = hour.getMinute();
+		int newHour = hour.getHour();
+		LocalTime rounded;
+		
+		if (newMinute > 0 && newMinute < 15) {
+			if (newMinute >= 8)
+				newMinute = 15;
+			else
+				newMinute = 0;
+		}else if (newMinute > 15 && newMinute < 30) {
+			if (newMinute >= 23)
+				newMinute = 30;
+			else
+				newMinute = 15;
+		}else if (newMinute > 30 && newMinute < 45) {
+			if (newMinute >= 38)
+				newMinute = 45;
+			else
+				newMinute = 30;
+		}else if (newMinute > 45 && newMinute <= 59) {
+			if (newMinute >= 53) {
+				newMinute = 0;
+				newHour += 1; 
+			}else
+				newMinute = 45;
+		}
+		
+		rounded = LocalTime.of(newHour, newMinute);
+		
+		return rounded;
 	}
 
-	public void setHour() {
-
+	public void setHour(LocalTime hour) {
+		this.hour = hour;
 	}
 
 	public String toString() {
-		return null;
+		return date.toString() + " " + hour.toString();
 	}
 
 }
