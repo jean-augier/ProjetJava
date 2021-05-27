5c1ed988e82a0d168a0fc9db9a656542174dd46d
Axel <axelraballand3@gmail.com>
2021-05-27 12:00:53 +0200

    controllerCheckinout

---
+++ src/controler/CheckInOutController.java
@@ -0,0 +1,10 @@
+package controler;
+
+import com.polyjava.poo.*;
+
+public class CheckInOutController {
+	public boolean createCheckInOut(String idEmp) {
+		CheckInOut check = new CheckInOut();
+		return true;
+	}
+}
