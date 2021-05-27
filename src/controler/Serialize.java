afe268423c941094649837ab5ec9cebb32052d97
Papy92 <clement.moreau92@gmail.com>
2021-05-27 11:30:15 +0200

    serialize done

--- src/controler/Serialize.java
+++ src/controler/Serialize.java
@@ -1,7 +1,9 @@
 package controler;
 
 import java.io.*;
-import com.polyjava.poo.CheckInOut;
+import java.util.ArrayList;
+
+import com.polyjava.poo.*;
 
 public class Serialize implements Serializable{
 	
@@ -27,28 +29,155 @@ public class Serialize implements Serializable{
 		fileName = newFileName;
 	}
 	
-	public void SerializeCheck(CheckInOut check) throws IOException
+	//to serialize CheckInOut
+	public void serializeCheck(CheckInOut check) throws IOException
 	{
 		// File initialize
 		fos = new FileOutputStream(getFileName());
 		oos = new ObjectOutputStream(fos);
 		
-		oos.writeObject(check.toString());
 		oos.writeObject(check);
+		
 		oos.close();
 		fos.close();
 	}
 	
-	// A modifier pour retourner l'objet et non le string;
-	public String getSerializeCheck() throws IOException, ClassNotFoundException
+	//to serialize CheckInOutList
+	public void serializeCheckList(ArrayList<CheckInOut> CheckInOutList) throws Exception
+	{
+		FileOutputStream fos = new FileOutputStream(getFileName());
+		ObjectOutputStream oos = new ObjectOutputStream(fos);
+		oos.writeObject(CheckInOutList);
+		oos.close();
+		fos.close();
+	}
+	
+	//to serialize Employee
+	public void serializeEmployee(Employee emp) throws IOException
+	{
+		// File initialize
+		fos = new FileOutputStream(getFileName());
+		oos = new ObjectOutputStream(fos);
+		
+		oos.writeObject(emp);
+		
+		oos.close();
+		fos.close();
+	}
+	
+	//to serialize EmployeeList
+	public void SerializeEmployeesList(ArrayList<Employee> Employees) throws Exception
+	{
+		FileOutputStream fos = new FileOutputStream(getFileName());
+		ObjectOutputStream oos = new ObjectOutputStream(fos);
+		oos.writeObject(Employees);
+		oos.close();
+		fos.close();
+	}
+	
+	//to serialize Department
+	public void SerializeDepartment(Department dpt) throws IOException
+	{
+		// File initialize
+		fos = new FileOutputStream(getFileName());
+		oos = new ObjectOutputStream(fos);
+		
+		oos.writeObject(dpt);
+		
+		oos.close();
+		fos.close();
+	}
+	
+	//to serialize DepartmentList
+	public void SerializeDepartmentList(ArrayList<Department> DepartmentList) throws Exception
+	{
+		FileOutputStream fos = new FileOutputStream(getFileName());
+		ObjectOutputStream oos = new ObjectOutputStream(fos);
+		oos.writeObject(DepartmentList);
+		oos.close();
+		fos.close();
+	}
+	
+	//to deserialize CheckInOut
+	public CheckInOut deserializeCheck() throws IOException, ClassNotFoundException
 	{
 		fis = new FileInputStream(getFileName());
 		ois = new ObjectInputStream(fis);
 		
-		String msg = (String) ois.readObject();
+		CheckInOut check = new CheckInOut();
+		check = (CheckInOut) ois.readObject();
 		ois.close();
 		fis.close();
 		
-		return msg;
+		return check;
+	}
+	
+	//to deserialize CheckInOutList
+	public ArrayList<CheckInOut> DeserializeCheckInOutList() throws Exception
+	{
+		ArrayList<CheckInOut> CheckInOutList;
+		
+		FileInputStream fis = new FileInputStream(getFileName());
+		ObjectInputStream ois = new ObjectInputStream(fis);
+		CheckInOutList = (ArrayList<CheckInOut>)ois.readObject();
+		ois.close();
+		
+		return CheckInOutList;
+	}
+	
+	//to deserialize Employee
+	public Employee deserializeEmployee() throws IOException, ClassNotFoundException
+	{
+		fis = new FileInputStream(getFileName());
+		ois = new ObjectInputStream(fis);
+		
+		Employee emp = new Employee();
+		emp = (Employee) ois.readObject();
+		ois.close();
+		fis.close();
+		
+		return emp;
+	}
+	
+	//to deserialize EmployeesList
+	public ArrayList<Employee> DeserializeEmployeesList() throws Exception
+	{
+		ArrayList<Employee> Employees;
+		
+		FileInputStream fis = new FileInputStream(getFileName());
+		ObjectInputStream ois = new ObjectInputStream(fis);
+		Employees = (ArrayList<Employee>)ois.readObject();
+		ois.close();
+		
+		return Employees;
+	}
+	
+	
+	
+	//to deserialize Department
+	public Department deserializeDepartment() throws IOException, ClassNotFoundException
+	{
+		fis = new FileInputStream(getFileName());
+		ois = new ObjectInputStream(fis);
+		
+		Department dpt = new Department();
+		dpt = (Department) ois.readObject();
+		ois.close();
+		fis.close();
+		
+		return dpt;
+	}
+
+	//to deserialize DepartmentList
+	public ArrayList<Department> DeserializeDepartmentList() throws Exception
+	{
+		ArrayList<Department> DepartmentList;
+		
+		FileInputStream fis = new FileInputStream(getFileName());
+		ObjectInputStream ois = new ObjectInputStream(fis);
+		DepartmentList = (ArrayList<Department>)ois.readObject();
+		ois.close();
+		
+		return DepartmentList;
 	}
 }
